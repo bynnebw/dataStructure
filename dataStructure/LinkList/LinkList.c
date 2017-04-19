7cb8193fcaae27c3308fb3ee39dd49efc1ed6b85
bryan <1365584663@qq.com>
2017-04-19 09:26:40 +0800

    revised some mistake

--- dataStructure/LinkList/LinkList.c
+++ dataStructure/LinkList/LinkList.c
@@ -1,3 +1,65 @@
 #include <stdio.h>
 #include <sys/malloc.h>
 #include "LinkList.h"
+
+typedef struct _tag_LinkList
+{
+	LinkListNode header;
+	int length;
+}TLinkList;
+
+LinkList* LinkList_Create()
+{
+	TLinkList* ret = (TLinkList*)malloc(sizeof(TLinkList));
+
+	if ( ret != NULL )
+	{
+		ret->length = 0;
+		ret->header.next = NULL;
+	}
+
+	return ret;
+}
+
+void LinkList_Destroy(LinkList* list)
+{
+	free(list);
+}
+
+void LinkList_Clear(LinkList* list)
+{
+	TLinkList* sList = (TLinkList*)list;
+
+	if ( sList != NULL )
+	{
+		sList->length = 0;
+		sList->header.next = NULL;
+	}
+}
+
+int LinkList_Length(LinkList* list)
+{
+	TLinkList* sList = (TLinkList*)list;
+	int ret = -1;
+
+	if ( sList != NULL )
+	{
+		ret = sList->length;
+	}
+	return ret;
+}
+
+int LinkList_Insert(LinkList* list,LinkListNode* node,int pos)
+{
+
+}
+
+LinkListNode* LinkList_Get(LinkList* list,int pos)
+{
+
+}
+
+LinkListNode* LinkList_Delete(LinkList* list,int pos)
+{
+
+}
