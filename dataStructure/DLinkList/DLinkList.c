f2be52fd4856ffc63b2d24e6698ddf57a00e8421
bryan <1365584663@qq.com>
2017-04-25 07:50:31 +0800

    There's still remains some of the code in the main.c to test if the code were right to work

--- dataStructure/DLinkList/DLinkList.c
+++ dataStructure/DLinkList/DLinkList.c
@@ -113,25 +113,117 @@ DLinkListNode* DLinkList_Get(DLinkList* list,int pos)
 
 DLinkListNode* DLinkList_Delete(DLinkList* list,int pos)
 {
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
+    int i = 0;
 
+    if ( (sList!=NULL) && (0 <= pos) && (pos < sList->length) )
+    {
+    	DLinkListNode* current = (DLinkListNode*)sList;		//这句话可以强行转换？
+    	DLinkListNode* next = NULL;
+
+    	for(i = 0;i < pos;i++)
+    	{
+    		current = current->next;
+    	}
+    	ret = current->next;	//这里感觉和普通的删除操作没有什么区别呢
+    	next = ret->next;
+    	current->next = next;
+
+    	if ( next!= NULL )
+    	{
+    		next->pre = current;
+
+    		if ( current == (DLinkListNode*)sList )
+    		{
+    			next->pre = NULL;
+    		}
+    	}
+
+    	if ( sList->slider == ret )			//再次出现了游标，该怎么理解呢？
+    	{
+    		sList->slider = next;
+    	}
+
+    	sList->length--;
+    }
+    return ret;
+}
+
+DLinkListNode* DLinkList_DeleteNode(DLinkList* list,DLinkListNode* node)
+{
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
+    int i = 0;
+
+    if ( sList!= NULL )
+    {
+    	DLinkListNode* current = (DLinkListNode*)sList;
+
+    	for(i=0;i<sList->length;i++)
+    	{
+    		if ( current->next == node )
+    		{
+    			ret = current->next;
+    			break;
+    		}
+    		current = current->next;
+    	}
+    	if ( ret != NULL )
+    	{
+    		DLinkList_Delete(sList,i);
+    	}
+    }
+    return ret;
 }
 
 DLinkListNode* DLinkList_Reset(DLinkList* list)
 {
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
 
+    if ( sList!= NULL )
+    {
+    	sList->slider = sList->header.next;
+    	ret = sList->slider;
+    }
+    return ret;
 }
 
 DLinkListNode* DLinkList_Current(DLinkList* list)
 {
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
 
+    if ( sList!= NULL )
+    {
+    	ret = sList->slider;
+    }
+    return ret;
 }
 
 DLinkListNode* DLinkList_Next(DLinkList* list)
 {
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
 
+    if ( (sList!=NULL) && (sList->slider != NULL) )	//这里还是存在疑问。获得下一个指针。
+    {
+    	ret = sList->slider;
+    	sList->slider = ret->next;
+    }
+    return ret;
 }
 
 DLinkListNode* DLinkList_Pre(DLinkList* list)
 {
+	TDLinkList* sList = (TDLinkList*)list;
+    DLinkListNode* ret = NULL;
 
+    if ( (sList!=NULL) && (sList->slider != NULL) )
+    {
+    	ret = sList->slider;
+    	sList->slider = ret->pre;
+    }
+    return ret;
 }
