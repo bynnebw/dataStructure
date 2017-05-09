5cc7157e2f48a469002e9d875faa9354b3a772b4
bryan <1365584663@qq.com>
2017-05-09 10:23:35 +0800

    I just add some contents of the code, and the key algorithm which I didn't finish

--- dataStructure/Sort/ShellSort.c
+++ dataStructure/Sort/ShellSort.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/*
+**	This is the basic idea of the ShellSort 
+**	Algoritm.
+** ShellSort is consisit of the InsertSortation
+** It feels more like the FFT algorithm
+*/
+
 void println(int array[],int len)
 {
 	int i = 0;
 
 	for (int i = 0; i < len; i++)
 	{
-		printf("%d", array[i]);
+		printf("%d ", array[i]);
 	}
 
 	printf("\n");
@@ -22,5 +29,8 @@ void swap(int array[], int i , int j)
 int main(int argc, char const *argv[])
 {
 	int array[] = {21,25,49,25,16,8};
+	int len = sizeof(array) / sizeof(*array);
+
+	println(array,len);	
 	return 0;
 }
